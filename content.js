a5d7a691e5defa9fca3f8fa5af65414a8c79ce5c
Saulo Developer <saulodesenvolvedor@gmail.com>
2020-04-18 14:39:27 -0300

    Adição de multiplas opções de aceleramento

--- content.js
+++ content.js
@@ -1,19 +1,44 @@
+// _9tCEa
+
 const interval = setInterval(() => {
   const header = document.querySelector("._3auIg");
   if (header) {
-    console.log(header);
     clearInterval(interval);
+    localStorage.setItem("playbackRate", 1);
 
     const button = document.createElement("button");
-    button.innerHTML = "2x";
+    button.innerHTML = "1x";
     button.classList.add("twoTimesButton");
 
     button.addEventListener("click", () => {
       const audio = document.querySelectorAll("audio");
-      audio.forEach((childItem) => {
-        console.log(childItem);
-        childItem.playbackRate = 2;
-      });
+
+      const styleButton = document.getElementsByClassName("twoTimesButton");
+      //   console.log(styleButton);
+
+      const playbackRateCurrent = localStorage.getItem("playbackRate");
+      //   console.log(playbackRateCurrent);
+
+      if (audio.length > 0) {
+        audio.forEach((childItem) => {
+          if (parseInt(playbackRateCurrent) + 1 < 5) {
+            localStorage.setItem(
+              "playbackRate",
+              String(parseInt(playbackRateCurrent) + 1)
+            );
+
+            childItem.playbackRate = parseInt(playbackRateCurrent) + 1;
+            button.innerHTML = `${parseInt(playbackRateCurrent) + 1}x`;
+            styleButton[0].style.color = "#ff4040";
+          } else {
+            localStorage.setItem("playbackRate", 1);
+
+            childItem.playbackRate = 1;
+            button.innerHTML = "1x";
+            styleButton[0].style.color = "#808080";
+          }
+        });
+      }
     });
 
     header.appendChild(button);
